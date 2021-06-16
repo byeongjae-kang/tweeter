8962d99ae3ba3ad7dcf2878c2e48f05e2ab00b9a
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-16 16:13:38 -0700

    refactor code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
-  console.log('Hello!');
-});
+  
+  console.log("ready");
 
-$("#tweet-text").keydown(function() {
-  const textLength = (140 - $(this).val().length);
-  if (textLength < 0) {
-    $('.counter').addClass("red").html(textLength);
-  } else {
-    $('.counter').removeClass("red").html(textLength);
-  }
+  $("#tweet-text").on('input', function() {
+    const textLength = (140 - $(this).val().length);
+    const $counter = $('.counter');
+    let isRed = false;
+    
+    if (textLength < 0 && !isRed) {
+      $counter.addClass("red");
+      isRed = true;
+    } else if (textLength >= 0 && isRed){
+      $counter.removeClass("red");
+      isRed = false;
+    }
+    $counter.html(textLength);
+  });
+
 });
 
