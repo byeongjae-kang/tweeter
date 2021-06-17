d31de38ad970e22ec74b15ddf6c7fce4680e6324
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-17 03:34:49 -0700

    modify code to remove bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
-  
-  console.log("ready");
 
   $("#tweet-text").on('input', function() {
     const textLength = (140 - $(this).val().length);
     const $counter = $('.counter');
-    let isRed = false;
     
-    if (textLength < 0 && !isRed) {
+    if (textLength < 0) {
       $counter.addClass("red");
-      isRed = true;
-    } else if (textLength >= 0 && isRed){
+    } else {
       $counter.removeClass("red");
-      isRed = false;
     }
     $counter.html(textLength);
   });
