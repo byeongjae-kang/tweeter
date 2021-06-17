5592671b68df7130cee16c743f1c442ea55c099f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-17 02:14:42 -0700

    add ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: 'GET',
+      dataType: 'json'
+    })
+    .then((posts) => {
+      console.log(posts);
+      renderTweets(posts);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
+  
+  loadTweets();
 
   $('form').submit(function(event) {
     event.preventDefault();
     const serialized = $(this).serialize();
   });
-
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1165116237227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
@@ -43,7 +32,6 @@ $(document).ready(() => {
       $('#tweets-container').append($tweet);
     }
   }
-
 
   const createTweetElement = function(tweet) {
     const { name, avatars, handle } = tweet.user;
@@ -77,77 +65,6 @@ $(document).ready(() => {
       </footer>
     </article>
     `);
-
     return $tweet;
   }
-
-  renderTweets(data);
-
 });
-
-
-    
-    // const fetchPosts = () => {
-      // $.ajax({
-        // url: `/tweets`,
-        // method: 'GET',
-        // dataType: 'json',
-        // success: (post) => {
-          // console.log(post);
-          // renderPosts(post);
-        // },
-        // error: (error) => {
-          // console.error(error);
-        // }
-      // });
-    // }
-// 
-    // const $postsButton = $('#fetch-posts');
-// 
-    // $postsButton.click(() => {
-      // fetchPosts();
-    // });
-// 
-    // const renderPosts = (posts) => {
-      // grab the 'posts' div
-      // const $posts = $('#posts');
-      // $posts.empty();
-      // create 'post' elements for each post
-      // for (const id in posts) {
-        // const post = posts[id];
-        // console.log(post);
-        // 
-        // const $post = $(`<div>`).addClass('posts');
-        // const $user = $('<h1>').text(post.user.name);
-        // const $content = $('<h1>').text(post.content.text);
-        // 
-        // $post.append($user, $content);
-// 
-        // $posts.append($post);
-      // }
-      // append to the DOM
-    // };
-
-
-
-
-
-
-// // Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
